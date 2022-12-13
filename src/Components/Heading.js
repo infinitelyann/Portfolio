724872b49b4998db577fe00cee4346c243502ad2
saparker <stephanieparker1997@gmail.com>
2022-12-13 18:31:58 -0500

    pfp

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -9,7 +9,7 @@ const Header = () => {
     return(
         <>
         <article className="container-sm" style={{marginTop: "15px" }}>
-        <img style={{width: "20%", borderRadius: "50%", border: "solid", float: "left", position: "absolute", zIndex: "3" }} src="https://i.imgur.com/RhdHDXT.jpg" alt="stephanie"/>
+        <img style={{width: "15%", borderRadius: "50%", border: "solid", float: "left", position: "absolute", zIndex: "3" }} src="https://i.imgur.com/RhdHDXT.jpg" alt="stephanie"/>
         <div style={{color: "white", backgroundColor: "black", opacity: ".75", borderRadius: "25px", position: "relative", zIndex: "1", textAlign: "right", padding:"10px"}}>
         <h4 className="sm-font">Stephanie Parker</h4>
         <h6 className="sm-font">Full-Stack Software Engineer</h6>
