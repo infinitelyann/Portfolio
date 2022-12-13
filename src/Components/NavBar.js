00bd5ef02305e9f98aa5b634ca211bacd818fee4
saparker <stephanieparker1997@gmail.com>
2022-12-13 10:49:42 -0500

    added dependencies to deploy

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,13 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+
 const Navigation = () => {
   return (
     <>
       <Navbar style={{ float: "right" }}>
         <Container>
           <Nav>
-            <Nav.Link>GitHub</Nav.Link>
+            <Nav.Link><a style={{textDecoration: "none", color: "inherit"}} href="https://github.com/infinitelyann" target="_blank">GitHub</a></Nav.Link>
             <Nav.Link>About</Nav.Link>
             <Nav.Link>Contact Me</Nav.Link>
           </Nav>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,25 +7,25 @@ const Projects = () => {
   return (
     
       <Container style={{display: "flex"}}>
-        <Card>
-            <img src="https://i.imgur.com/QcM51DE.png" alt="BlackJack"/>
-          <Card.Body ></Card.Body>
-          <Card.Footer><a href="https://infinitelyann.github.io/Project-1/">BlackJack</a></Card.Footer>
+        <Card className="opacity-containers">
+            
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/QcM51DE.png" alt="BlackJack"/></Card.Body>
+          <Card.Footer><a style={{color: "white"}} href="https://infinitelyann.github.io/Project-1/">BlackJack</a></Card.Footer>
         </Card>
-        <Card>
-        <img src="https://i.imgur.com/HVcQkTQ.png" alt="BlogPost"/>
-          <Card.Body></Card.Body>
-          <Card.Footer><a href="https://angstyblog.fly.dev/">Angsty BlogPost</a></Card.Footer>
+        <Card className="opacity-containers">
+        
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/HVcQkTQ.png" alt="BlogPost"/></Card.Body>
+          <Card.Footer><a style={{color: "white"}} href="https://angstyblog.fly.dev/" target="_blank">Angsty BlogPost</a></Card.Footer>
         </Card>
-        <Card>
-        <img  src="https://i.imgur.com/Wm2if6H.png" alt="Quizard"/>
-          <Card.Body></Card.Body>
-          <Card.Footer><a href="https://quizard-by-tuneheads.netlify.app/">Quizard</a></Card.Footer>
+        <Card className="opacity-containers">
+        
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/Wm2if6H.png" alt="Quizard"/></Card.Body>
+          <Card.Footer><a style={{color: "white"}} href="https://quizard-by-tuneheads.netlify.app/" target="_blank">Quizard</a></Card.Footer>
         </Card>
-        <Card>
-        <img src="https://i.imgur.com/2VZub1C.png" alt="blogr"/>
-          <Card.Body></Card.Body>
-          <Card.Footer>blogr</Card.Footer>
+        <Card className="opacity-containers">
+        
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/2VZub1C.png" alt="blogr"/></Card.Body>
+          <Card.Footer><a style={{color: "white"}} href="https://github.com/infinitelyann/Capstone-Client" target="_blank">blogr</a></Card.Footer>
         </Card>
       </Container>
 
