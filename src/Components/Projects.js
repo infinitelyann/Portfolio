544f812a6711cbfd6b9790860b3a78d7009da653
saparker <stephanieparker1997@gmail.com>
2022-12-13 16:42:57 -0500

    added gifs to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,14 +10,14 @@ const Projects = () => {
         <Card className="opacity-containers">
       <a style={{color: "white"}} href="https://infinitelyann.github.io/Project-1/">
             
-          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/QcM51DE.png" alt="BlackJack"/></Card.Body>
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/ifGTsZt.gif"/> </Card.Body>
           <Card.Footer>BlackJack</Card.Footer>
         </a>
         </Card>
         <Card className="opacity-containers">
         <a style={{color: "white"}} href="https://angstyblog.fly.dev/" target="_blank">
         
-          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/HVcQkTQ.png" alt="BlogPost"/></Card.Body>
+          <Card.Body><img style={{width: "150px"}} src="https://i.imgur.com/bnPd5Gy.gif" alt="BlogPost"/></Card.Body>
           <Card.Footer>Angsty BlogPost</Card.Footer>
         </a>
         </Card>
