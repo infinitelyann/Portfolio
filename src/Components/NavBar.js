bdbf94c9548962593eb8f2a5f5e9e84b99872c6e
saparker <stephanieparker1997@gmail.com>
2022-12-17 22:52:23 -0500

    added project descriptions

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -15,7 +15,7 @@ const Navigation = () => {
           <Nav>
             <a className="nav-links" href="https://github.com/infinitelyann" target="_blank">GitHub</a>
             <a className="nav-links" href="https://www.linkedin.com/in/stephanie-a-parker/" target="_blank">LinkedIn</a>
-            <a className="nav-links" href="https://docs.google.com/document/d/1arCYtD_x2iWU9k4sYe8V4jxN2j5b18ClRk4r5p8PJ6Y/edit" target="_blank">Resume</a>
+            <a className="nav-links" href="https://docs.google.com/document/d/1arCYtD_x2iWU9k4sYe8V4jxN2j5b18ClRk4r5p8PJ6Y/export?format=pdf" target="_blank">Resume</a>
             
           </Nav>
         </Container>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,30 +8,62 @@ const Projects = () => {
     <>
       <Container style={{display: "flex", paddingTop: "5%", justifyContent: "center", marginBottom: "5%"}}>
         <Card className="opacity-containers">
-      <a style={{color: "white"}} href="https://infinitelyann.github.io/Project-1/">
+      <a className="projects" style={{color: "white"}} href="https://infinitelyann.github.io/Project-1/">
             
-          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/ifGTsZt.gif"/> </Card.Body>
+          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/ifGTsZt.gif"/>
+           </Card.Body>
           <Card.Footer style={{textAlign: "center"}}>BlackJack</Card.Footer>
         </a>
+        <Card.Body className="description">
+          <p>A simplified take on the casino classic card game of BlackJack- dueling against the dealer(computer)
+to get the sum of your randomized hand to 21- without going over!
+<br/>
+-Implementation, manipulation and rendering of data from <a href="https://www.deckofcardsapi.com/">Deck of Cards API</a>
+<br/>
+-Features HTML, CSS3, JavaScript (DOM Manipulation)
+<br/>
+-Follows best development practices</p>
+          </Card.Body>
         </Card>
         <Card className="opacity-containers">
-        <a style={{color: "white"}} href="https://angstyblog.fly.dev/" target="_blank">
+        <a className="projects" style={{color: "white"}} href="https://angstyblog.fly.dev/" target="_blank">
         
-          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/bnPd5Gy.gif" alt="BlogPost"/></Card.Body>
+          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/bnPd5Gy.gif" alt="BlogPost"/>
+          </Card.Body>
           <Card.Footer style={{textAlign: "center"}}>Angsty BlogPost</Card.Footer>
         </a>
+        <Card.Body className="description">
+          <p>An application allowing users to sign-in/up, choose a theme for a blog-style text post from
+two different color pickers; a foreground/text color and a background color, and a photo.
+<br/>
+-Features HTML, CSS3, Express.js, LiquidJS, Mongoose, Bootstrap</p>
+          </Card.Body>
         </Card>
         <Card className="opacity-containers">
-        <a style={{color: "white"}} href="https://quizard-by-tuneheads.netlify.app/" target="_blank">
-          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/3Vbho2k.gif" alt="Quizard"/></Card.Body>
+        <a className="projects" style={{color: "white"}} href="https://quizard-by-tuneheads.netlify.app/" target="_blank">
+          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/3Vbho2k.gif" alt="Quizard"/>
+          </Card.Body>
           <Card.Footer style={{textAlign: "center"}}>Quizard</Card.Footer>
           </a>
+          <Card.Body className="description">
+          <p>A trivia game that allows users to create and play their own trivia game. 
+            <br/>This application also allows
+the user to play a trivia game from an API call that implements the user's input for category, difficulty, and
+amount of questions. Give it a try and see how well you do!
+<br/>
+-Implementation, manipulation and rendering of data from <a href="https://opentdb.com/">Open Trivia Database</a>
+<br/>
+-Features React, Mongoose, Express.js, Bootstrap, HTML and CSS3</p>
+          </Card.Body>
         </Card>
         <Card className="opacity-containers">
-        <a style={{color: "white"}} href="https://github.com/infinitelyann/Capstone-Client" target="_blank">
-          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/rESn5A2.gif" alt="blogr"/></Card.Body>
+        <a className="projects" style={{color: "white"}} href="https://github.com/infinitelyann/Capstone-Client" target="_blank">
+          <Card.Body><img style={{width: "75%"}} src="https://i.imgur.com/rESn5A2.gif" alt="blogr"/>
+          </Card.Body>
           <Card.Footer style={{textAlign: "center" }}>blogr</Card.Footer>
           </a>
+          <Card.Body className="description">
+          </Card.Body>
         </Card>
        
       </Container>
